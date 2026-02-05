2161b2c921903d8f5dbbab983b19c2e4da028e02
gnoel2825 <gabriellenoel@Gabrielles-MBP.lan>
2026-02-05 15:12:20 -0500

    new fix

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -11,9 +11,10 @@ else
     same_site: :lax
 end
 
-Rails.application.config.session_store :cookie_store,
+  Rails.application.config.session_store :cookie_store,
   key: "_project_skate_session",
   same_site: :none,
-  secure: Rails.env.production?
+  secure: true
+
 
 
