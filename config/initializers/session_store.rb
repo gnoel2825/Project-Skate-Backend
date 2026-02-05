82d5850bb6cc5d279d3bfda2ffde1e9995273273
gnoel2825 <gabriellenoel@Gabrielles-MBP.lan>
2026-02-05 15:13:43 -0500

    please fix it

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,20 +1,4 @@
-
-if Rails.env.production?
-  Rails.application.config.session_store :cookie_store,
-    key: "_authentication_app",
-    domain: "glisse-frontend.onrender.com/",
-    secure: true,
-    same_site: :lax
-else
-  Rails.application.config.session_store :cookie_store,
-    key: "_authentication_app",
-    same_site: :lax
-end
-
-  Rails.application.config.session_store :cookie_store,
+Rails.application.config.session_store :cookie_store,
   key: "_project_skate_session",
   same_site: :none,
-  secure: true
-
-
-
+  secure: Rails.env.production?
